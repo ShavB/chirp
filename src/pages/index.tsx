0e9fd272e288badac1aafb7edaa0804eebb2e8e7
Shyam Bisht <bishtshyam135@gmail.com>
2023-07-01 02:41:38 +0530

    Added post router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,19 @@ import { SignIn, SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 import { api } from "~/utils/api";
 
 export default function Home() {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
   const user = useUser();
+  const { data } = api.posts.getAll.useQuery();
   return (
     <>
-    {!user.isSignedIn && <SignInButton />}
-    {!!user.isSignedIn && <SignOutButton />}
+      <div>
+        {!user.isSignedIn && <SignInButton />}
+        {!!user.isSignedIn && <SignOutButton />}
+      </div>
+      <div>
+        {data?.map((post) => (
+          <div key={post.id} >{post.content}</div>
+        ))}
+      </div>
     </>
   );
 }
